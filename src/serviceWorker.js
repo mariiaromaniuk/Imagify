fe0d24411be0020a42f1268680a4693c8e470635
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2023-02-21 14:33:22 -0800

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -25,8 +25,8 @@ export function register(config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
+      // The service worker won't work if PUBLIC_URL is on a different origin
+      // from what the page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
